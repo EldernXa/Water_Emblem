a7734cbe87356a6cf35e46902ffc054b86d360e1
anthony <anthony_pro1998@outlook.fr>
2020-05-14 15:21:39 +0200

    modify Plaine

--- src/main/java/backend/Plaine.java
+++ src/main/java/backend/Plaine.java
@@ -1,6 +1,12 @@
 package backend;
 
+import javafx.scene.paint.Color;
+
 public class Plaine extends Field {
+
+    public Plaine(){
+        super(Color.WHITE);
+    }
 
     void affect(Personnage personnage) {
 
