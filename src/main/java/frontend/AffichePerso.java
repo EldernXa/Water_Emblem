e46afc52774f57c152cb34052435511e404214d2
Oswinne <johan.lorenzatti@etu.univ-amu.fr>
2020-05-16 15:11:15 +0200

    rien de spécial

--- src/main/java/frontend/AffichePerso.java
+++ src/main/java/frontend/AffichePerso.java
@@ -26,7 +26,7 @@ public class AffichePerso {
         perso.setVgap(50);
         perso.setHgap(50);
         try {
-            FileInputStream inputStream = new FileInputStream("./src/main/resources/spritesPersos/SrpriteEliwwod/EliwoodAvant1.png");
+            FileInputStream inputStream = new FileInputStream("src/main/resources/spritesPersos/SpriteEliwood/EliwoodAvant1.png");
             Image img = new Image(inputStream);
             imgView = new ImageView(img);
             imgView.setFitHeight(50);
