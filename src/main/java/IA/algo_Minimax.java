a9d8204b5242b6696ffc43a833490ff96c262a8a
charles <charles.tavenard@etu.univ-amu.fr>
2020-05-17 15:22:32 +0200

    optimisation de minimax

--- src/main/java/IA/algo_Minimax.java
+++ src/main/java/IA/algo_Minimax.java
@@ -3,7 +3,35 @@ package IA;
 public class algo_Minimax {
 
 
-    static int minimax(Etat etat, int depth, boolean maxi){
+    static public Etat startMini(Etat etat, int depth, boolean maxi){
+        int value ;
+        Etat etatFin = etat;
+        if(maxi){
+            value = -100000;
+            for (Etat e : etat.getToutPossibilité(maxi)){
+                int x =  minimax(e, depth - 1, !maxi);
+                if(x > value){
+                    value = x;
+                    etatFin = e;
+                }
+            }
+            return etatFin;
+        }
+
+        else{
+            value = 100000;
+            for (Etat e : etat.getToutPossibilité(maxi)){
+                int x =  minimax(e, depth - 1, !maxi);
+                if(x < value){
+                    value = x;
+                    etatFin = e;
+                }
+            }
+            return etatFin;
+        }
+    }
+
+    private static int minimax(Etat etat, int depth, boolean maxi){
         int value;
         if(depth == 0 || etat.estFinal()){
             return etat.getValHeuristique();
