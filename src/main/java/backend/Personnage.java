d59e08a97ca558f9fd54e631680f97f1ae480240
sykom1 <saber.tnifas@etu.univ-amu.fr>
2020-05-17 12:12:42 +0200

    suppression de la classe calculstat et rajout d'une méthode dans la classe personnage

--- src/main/java/backend/Personnage.java
+++ src/main/java/backend/Personnage.java
@@ -65,4 +65,30 @@ public class Personnage {
     public void setPos(Coordinate pos) {
         this.pos = pos;
     }
+
+    public void calculerStat(){
+        Carac caracPerso = this.getCaracteristique();
+        String arme1 = caracPerso.getWep1();
+        int str = caracPerso.getStr();
+        switch (arme1){
+            case "Epee":{
+                caracPerso.setStr(str + 4);
+            }
+            case "Lance": {
+                caracPerso.setStr(str + 3);
+                caracPerso.setPorte(2);
+            }
+            case "Hache": {
+                caracPerso.setStr(str + 2);
+            }
+            case "Arc" : {
+                caracPerso.setStr(str + 2);
+                caracPerso.setPorte(5);
+            }
+            case "Magie" : {
+                caracPerso.setPorte(3);
+            }
+        }
+    }
+
 }
