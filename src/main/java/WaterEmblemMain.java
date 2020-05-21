1bfe00e6216492a19690b3c5e7627fa3c6691e8f
anthony <anthony_pro1998@outlook.fr>
2020-05-21 16:12:44 +0200

    ............................

--- src/main/java/WaterEmblemMain.java
+++ src/main/java/WaterEmblemMain.java
@@ -3,11 +3,14 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 
 public class WaterEmblemMain extends Application {
     public void start(Stage primaryStage)
     {
-        primaryStage.setScene(new Scene(new AffichageGraphique().init(), primaryStage.getMaxWidth(), primaryStage.getMaxHeight()));
+        Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        primaryStage.setScene(new Scene(new AffichageGraphique().init(), (int)dimension.getWidth(), (int)dimension.getHeight()));
         primaryStage.show();
     }
     public static void main(String[] args){
