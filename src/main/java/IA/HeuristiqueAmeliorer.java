98ac283cb07c03e939eb8ea3ab0928cc34ae369a
charles <charles.tavenard@etu.univ-amu.fr>
2020-05-23 21:24:23 +0200

    heuristique

--- src/main/java/IA/HeuristiqueAmeliorer.java
+++ src/main/java/IA/HeuristiqueAmeliorer.java
@@ -8,46 +8,40 @@ public class HeuristiqueAmeliorer implements Heuristique {
 
     @Override
     public int calculerHeuristique(Etat e) {
-        int value = 0;
-        int som = 0;
-        int x;
-        int y;
-        int min = 0;
-        int valFinal = 0;
+        int x,y ;
+        int min = 10000;
+        int distance = 0;
+        int valueG = 0;
+        int valueM = 0;
+        for (Personnage p : e.getListMechant()){
+            for (Personnage g : e.getGentils()){
+                x = Math.abs(p.getPos().getX() - g.getPos().getX());
+                y = Math.abs(p.getPos().getY() - g.getPos().getY());
+                if(min > x + y){
+                    min = x + y;
+                }
+            }
+            distance += min;
+        }
+
         for (Personnage persoG : e.getGentils()){
-            value += persoG.getCaracteristique().getHp();
-            for (Personnage p : e.getListMechant()){
-                x = Math.abs(p.getPos().getX() - persoG.getPos().getX());
-                y = Math.abs(p.getPos().getY() - persoG.getPos().getY());
-                if(min > x + y){
-                    min = x + y;
-                }
-            }
+            valueG += persoG.getCaracteristique().getHp();
         }
-        valFinal += 50 - min ;
-        min = 0;
+
         for (Personnage persoM : e.getListMechant()){
-            som += persoM.getCaracteristique().getHp();
-            for (Personnage p : e.getListMechant()){
-                x = Math.abs(p.getPos().getX() - persoM.getPos().getX());
-                y = Math.abs(p.getPos().getY() - persoM.getPos().getY());
-                if(min > x + y){
-                    min = x + y;
-                }
-            }
+            valueM += persoM.getCaracteristique().getHp();
         }
-        valFinal += 50 - min;
-        if(som == 0 && value == 0){
+
+        if(valueG == 0 && valueM == 0){
             return 0;
         }
-        if(som == 0){
+        if(valueM == 0){
             return 10000;
         }
-        if(value == 0){
+        if(valueG == 0){
             return -10000;
         }
-        valFinal += value - som;
 
-        return valFinal;
+        return valueG - valueM + distance;
     }
 }
