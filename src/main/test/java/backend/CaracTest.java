bdbe91b5626848b80a26a72e81c855d3b1c9c206
charles <charles.tavenard@etu.univ-amu.fr>
2020-05-26 12:53:43 +0200

    test Carac

--- src/main/test/java/backend/CaracTest.java
+++ src/main/test/java/backend/CaracTest.java
@@ -1,168 +1,31 @@
 package backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CaracTest {
-
     @Test
-    void getMaxSpd() {
-    }
-
-    @Test
-    void setMaxSpd() {
-    }
-
-    @Test
-    void getMaxLck() {
-    }
-
-    @Test
-    void setMaxLck() {
+    void testConstructeur() {
+        Carac carac = new Carac("Marth");
+        Assertions.assertEquals(carac.getName(),"Marth");
+        Assertions.assertEquals(carac.getMaxHp(),17);
     }
 
     @Test
     void cloner() {
+        Carac carac = new Carac("Marth");
+        Carac clone = carac.cloner();
+
+        Assertions.assertEquals(carac.getMov(), clone.getMov());
+        Assertions.assertEquals(carac.getSpd(), clone.getSpd());
+        Assertions.assertEquals(carac.getHp(), clone.getHp());
+
+        clone.setHp(300);
+        Assertions.assertEquals(clone.getHp(), 300);
+        Assertions.assertNotEquals(carac.getHp(), clone.getHp());
     }
 
-    @Test
-    void getName() {
-    }
 
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void getType() {
-    }
-
-    @Test
-    void setType() {
-    }
-
-    @Test
-    void getMaxHp() {
-    }
-
-    @Test
-    void setMaxHp() {
-    }
-
-    @Test
-    void getHp() {
-    }
-
-    @Test
-    void setHp() {
-    }
-
-    @Test
-    void getStr() {
-    }
-
-    @Test
-    void setStr() {
-    }
-
-    @Test
-    void getDef() {
-    }
-
-    @Test
-    void setDef() {
-    }
-
-    @Test
-    void getMag() {
-    }
-
-    @Test
-    void setMag() {
-    }
-
-    @Test
-    void getSkl() {
-    }
-
-    @Test
-    void setSkl() {
-    }
-
-    @Test
-    void getLck() {
-    }
-
-    @Test
-    void setLck() {
-    }
-
-    @Test
-    void getRes() {
-    }
-
-    @Test
-    void setRes() {
-    }
-
-    @Test
-    void getMov() {
-    }
-
-    @Test
-    void setMov() {
-    }
-
-    @Test
-    void getWep1() {
-    }
-
-    @Test
-    void setWep1() {
-    }
-
-    @Test
-    void getWep2() {
-    }
-
-    @Test
-    void setWep2() {
-    }
-
-    @Test
-    void getWep3() {
-    }
-
-    @Test
-    void setWep3() {
-    }
-
-    @Test
-    void getPortee() {
-    }
-
-    @Test
-    void setPortee() {
-    }
-
-    @Test
-    void getMaxMov() {
-    }
-
-    @Test
-    void setMaxMov() {
-    }
-
-    @Test
-    void getSpd() {
-    }
-
-    @Test
-    void setSpd() {
-    }
-
-    @Test
-    void getDeplacement() {
-    }
 }
