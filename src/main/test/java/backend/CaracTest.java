969ac822666dafd3566feb24076144276de3813e
anthony <anthony_pro1998@outlook.fr>
2020-05-27 15:24:55 +0200

    fix move on uncrossable field

--- src/main/test/java/backend/CaracTest.java
+++ src/main/test/java/backend/CaracTest.java
@@ -1,31 +1,8 @@
-package backend;
+package java.backend;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CaracTest {
-    @Test
-    void testConstructeur() {
-        Carac carac = new Carac("Marth");
-        Assertions.assertEquals(carac.getName(),"Marth");
-        Assertions.assertEquals(carac.getMaxHp(),17);
-    }
-
-    @Test
-    void cloner() {
-        Carac carac = new Carac("Marth");
-        Carac clone = carac.cloner();
-
-        Assertions.assertEquals(carac.getMov(), clone.getMov());
-        Assertions.assertEquals(carac.getSpd(), clone.getSpd());
-        Assertions.assertEquals(carac.getHp(), clone.getHp());
-
-        clone.setHp(300);
-        Assertions.assertEquals(clone.getHp(), 300);
-        Assertions.assertNotEquals(carac.getHp(), clone.getHp());
-    }
+public class CaracTest {
 
 
 }
