d256f3894aaf5aa6173b55a1158b74ab407d96fe
charles <charles.tavenard@etu.univ-amu.fr>
2020-05-28 00:56:46 +0200

    supp les test inutile

--- src/main/test/java/backend/data/DataPersoTest.java
+++ src/main/test/java/backend/data/DataPersoTest.java
@@ -8,24 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class DataPersoTest {
 
     @Test
-    void createUnit() {
-    }
-
-    @Test
-    void createCharacterUnit() {
-    }
-
-    @Test
-    void getUnite() {
-
-
-    }
-
-    @Test
-    void getCharacter() {
-    }
-
-    @Test
     void getCharacterLine() {
 
         String mercenaire = "Lyn;Epeiste;21;8;1;11;10;7;4;2;6;Epee";
@@ -38,22 +20,4 @@ class DataPersoTest {
         Assertions.assertEquals(mercenaire,DataPerso.getUniteLine("Mercenaire"));
     }
 
-    @Test
-    void getStatsCharacter() {
-        Assertions.assertEquals(DataPerso.getStatsCharacter("Lyn","hp"),"21");
-    }
-
-    @Test
-    void getStatsUnite() {
-        Assertions.assertEquals(DataPerso.getStatsUnite("Mercenaire","hp"),"18");
-    }
-
-    @Test
-    void setCharacterStats() {
-    }
-
-    @Test
-    void getFightingType() {
-        Assertions.assertEquals(DataPerso.getFightingType("Lyn"),0);
-    }
 }
